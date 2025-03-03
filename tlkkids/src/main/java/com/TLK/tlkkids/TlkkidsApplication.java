48bcaa4c7a1da9d8693d67409d9f8ed007691f6c
Matheus Brenner <108211843+MathiasBelmont@users.noreply.github.com>
2025-03-03 14:19:03 -0300

    Add files via upload

---
+++ tlkkids/src/main/java/com/TLK/tlkkids/TlkkidsApplication.java
@@ -0,0 +1,13 @@
+package com.TLK.tlkkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlkkidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlkkidsApplication.class, args);
+	}
+
+}
