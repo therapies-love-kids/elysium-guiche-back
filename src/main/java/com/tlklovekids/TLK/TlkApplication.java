d1d97683f878de13e3f8229b9421071187f4f05f
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-03 09:08:34 -0300

    commit incial

---
+++ src/main/java/com/tlklovekids/TLK/TlkApplication.java
@@ -0,0 +1,13 @@
+package com.tlklovekids.TLK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlklovekids/TLK/TlkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlklovekids.TLK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
